423b92693a9a392ab670676db77376d5963d8c56
Michelcalex <Michelcalex+github@gmail.com>
2017-01-08 15:09:59 -0500

    Re-wrote getFoods function to use mustache template and push new items to empty array. Displays new items in DOM

--- public/app.js
+++ public/app.js
@@ -31,38 +31,82 @@ window.addEventListener('load', function() {
 },{"./food":2,"./menu":3,"./search":4}],2:[function(require,module,exports){
 console.log('I am working food');
 
+let menuItems = [];
+
 
 function getFoods() {
     let submitBtn = document.querySelector('#submit-btn');
 
     submitBtn.addEventListener('click', function() {
+        let foodName = document.querySelector('#food-name');
+        let foodDescription = document.querySelector('#food-description');
+        let foodPrice = document.querySelector('#price');
+
+        let newItem = {
+            name: 'Name: ' + foodName.value,
+            description: 'Description: ' + foodDescription.value,
+            price: 'Price: $' + foodPrice.value,
+        };
+        
+        menuItems.push(newItem);
 
         let menuList = document.querySelector('#menu-list');
+        let item = document.createElement('li');
+        for (let i = 0; i < menuItems.length; i++) {
 
-        let foodName = document.querySelector('#food-name');
-        let foodItem = document.createElement('li');
-        foodItem.textContent = 'Name: ' + foodName.value;
-        menuList.appendChild(foodItem);
+            item.innerHTML = Mustache.render (
+                document.querySelector('#food-list-template').innerHTML, 
+                {
+                    name: menuItems[i].name, 
+                    description: menuItems[i].description,
+                    price: menuItems[i].price,
+                }
+            );
 
-        let foodDescription = document.querySelector('#food-description');
-        let descriptionItem = document.createElement('li');
-        descriptionItem.textContent = 'Description: ' + foodDescription.value;
-        menuList.appendChild(descriptionItem);
+            menuList.appendChild(item);
+        }
 
-        let foodPrice = document.querySelector('#price');
-        let priceItem = document.createElement('li');
-        priceItem.textContent = 'Price: ' + foodPrice.value;
-        menuList.appendChild(priceItem);
-
-        foodName.value= '';
-        foodDescription.value= '';
-        foodPrice.value= '';
+        foodName.value = '';
+        foodDescription.value = '';
+        foodPrice.value = '';
     });
 }
 
+console.log(menuItems);
 
 module.exports = getFoods;
 
+
+
+
+// ***********Manipulating DOM 
+// function getFoods() {
+//     let submitBtn = document.querySelector('#submit-btn');
+
+//     submitBtn.addEventListener('click', function() {
+
+//         let menuList = document.querySelector('#menu-list');
+
+//         let foodName = document.querySelector('#food-name');
+//         let foodItem = document.createElement('li');
+//         foodItem.textContent = 'Name: ' + foodName.value;
+//         menuList.appendChild(foodItem);
+
+//         let foodDescription = document.querySelector('#food-description');
+//         let descriptionItem = document.createElement('li');
+//         descriptionItem.textContent = 'Description: ' + foodDescription.value;
+//         menuList.appendChild(descriptionItem);
+
+//         let foodPrice = document.querySelector('#price');
+//         let priceItem = document.createElement('li');
+//         priceItem.textContent = 'Price: ' + foodPrice.value;
+//         menuList.appendChild(priceItem);
+
+//         foodName.value= '';
+//         foodDescription.value= '';
+//         foodPrice.value= '';
+//     });
+// }
 },{}],3:[function(require,module,exports){
 console.log('I am working menu');
 },{}],4:[function(require,module,exports){
