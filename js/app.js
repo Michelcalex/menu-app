84c406f2f8df53a576005451dd564a0dc564c1fb
Michelcalex <Michelcalex+github@gmail.com>
2017-01-08 11:24:35 -0500

    Added js folder and created three modules: food, menu, search

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//display hidden class
+
+
+let food = require('./food');
+let menu = require('./menu');
+let search = require('./search');
+
+
+
+window.addEventListener('load', function() {
+    console.log('I am working');
+});
